8ea18f6e159e3ceee435bef9563a5f60925a0a59
ycc <758649277@qq.com>
2021-05-14 23:01:23 +0800

    新增controller

---
+++ src/StudentController.java
@@ -0,0 +1,8 @@
+public class StudentController {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setAge(1);
+        student.setName("zs");
+
+    }
+}
